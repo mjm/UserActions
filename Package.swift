72348f861b4491fa5a9fce08a698e52b30344a43
Matt Moriarity <matt@mattmoriarity.com>
2019-11-11 23:49:50 -0600

    Switch to fork of Entwine

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/mjm/Events", .branch("master")),
         .package(url: "https://github.com/mjm/Combinable", .branch("master")),
-        .package(url: "https://github.com/tcldr/Entwine", from: "0.8.0"),
+        .package(url: "https://github.com/mjm/Entwine", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
